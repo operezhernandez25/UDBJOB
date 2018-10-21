9898d39ffb10d18efc2bc970e60a963cebf61d34
eduardosm1 <43509658+eduardosm1@users.noreply.github.com>
2018-10-21 16:41:58 -0600

    Add files via upload

---
+++ application/models/MRegistroE.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class MRegistroE extends CI_Model {
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+  public function registrar($data)
+  {
+    return $this->db->insert("usuarioEmpresa",$data);
+  }
+
+	public function InsertEmpresa($data)
+	{
+		return $this->db->insert('empresa',$data);
+	}
+
+	public function idEmpresa($nombre)
+	{
+		$this->db->where("nombre",$nombre);
+		$query = $this->db->get("empresa");
+		return $query->result();
+	}
+
+}
