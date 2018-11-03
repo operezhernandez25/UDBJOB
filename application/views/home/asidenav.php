16edc6d2d9898116219b331b6fc02fedc4468c14
Oscar Perez <operezhernandez25@gmail.com>
2018-11-02 23:19:14 -0600

    fix oscar

--- application/views/home/asidenav.php
+++ application/views/home/asidenav.php
@@ -194,9 +194,16 @@
 
               <!-- Menu Footer-->
               <li class="user-footer">
-                <div class="pull-left">
+                <?php
+                  if($this->session->userdata("s_tipo")==1)
+                  {
+                    ?>
+                    <div class="pull-left">
                   <a href="<?php echo base_url(); ?>perfil" class="btn btn-default btn-flat">Perfil</a>
                 </div>
+                    <?php
+                  }
+                ?>
                 <div class="pull-right">
                   <a href="<?php echo base_url(); ?>logout" class="btn btn-default btn-flat" id="btncerrarsesion">Cerrar Sesión</a>
                 </div>
